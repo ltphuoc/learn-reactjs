20c7a44f16e51bd47d40dc2a680604200df375ad
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2022-04-14 20:38:48 +0700

    Update Todo component

--- src/components/Todo/AddTodo.js
+++ src/components/Todo/AddTodo.js
@@ -10,7 +10,7 @@ const AddTodo = (props) => {
           handleSubmit();
         }}
       >
-        Submit
+        Add
       </button>
     </div>
   );
--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -9,6 +9,7 @@ function Todo() {
     return JSON.parse(localStorage.getItem('list-todo')) ?? [];
   });
 
+  const [filterStatus, setFilterStatus] = useState('all');
   const handleSubmit = () => {
     if (!todo) return;
     let todoId = listTodo.length + 1;
@@ -44,11 +45,31 @@ function Todo() {
     setListTodo(newTodoList);
     localStorage.setItem('list-todo', JSON.stringify(newTodoList));
   };
+
+  const handleShowAllClick = () => {
+    setFilterStatus('all');
+  };
+  const handleShowCompletedClick = () => {
+    setFilterStatus('completed');
+  };
+  const handleShowOngoingClick = () => {
+    setFilterStatus('pending');
+  };
+
+  const renderedListTodo = listTodo.filter(
+    (todo) => filterStatus === 'all' || filterStatus === todo.status
+  );
+
   return (
     <div>
+      <div className="">
+        <button onClick={handleShowAllClick}>Show All</button>
+        <button onClick={handleShowCompletedClick}>Show Completed</button>
+        <button onClick={handleShowOngoingClick}>Show Ongoing</button>
+      </div>
       <AddTodo todo={todo} setTodo={setTodo} handleSubmit={handleSubmit} />
       <DisplayTodo
-        listTodo={listTodo}
+        listTodo={renderedListTodo}
         handleDoneTodo={handleDoneTodo}
         handleDeleteTodo={handleDeleteTodo}
       />
